b2ebc8fd7bb6616e495f41177b8efd989f2c8ee6
janejoahnna <janejoahnna@gmail.com>
2024-10-18 05:13:19 +0800

    fifth commit

--- routes/post.js
+++ routes/post.js
@@ -5,13 +5,13 @@ const auth = require('../auth');
 
 // Admin-only routes
 router.post('/addPost', auth.verifyToken, auth.verifyAdmin, postController.createPost);
-router.put('/updatePost/:id', auth.verifyToken, auth.verifyAdmin, postController.updatePost);
-router.delete('/deletePost/:id', auth.verifyToken, auth.verifyAdmin, postController.deletePost);
+router.put('/updatePost/:postId', auth.verifyToken, auth.verifyAdmin, postController.updatePost);
+router.delete('/deletePost/:postId', auth.verifyToken, auth.verifyAdmin, postController.deletePost);
 
 // Authenticated user routes
 router.get('/getPosts', auth.verifyToken, postController.getPosts);
-router.get('/getPost/:id', auth.verifyToken, postController.getPostById);
-router.post('/addComment/:id', auth.verifyToken, postController.addComment);
-router.get('/getComments/:id', auth.verifyToken, postController.getComments);
+router.get('/getPost/:postId', auth.verifyToken, postController.getPostById);
+router.post('/addComment/:postId', auth.verifyToken, postController.addComment);
+router.get('/getComments/:postId', auth.verifyToken, postController.getComments);
 
 module.exports = router;
